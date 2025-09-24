38f3dd8edb7beb1f40043d07e627609488e5ad80
satwika3 <2300030465@kluniversity.in>
2025-09-25 00:16:36 +0530

    Updated code with latest changes

--- frontend/dockerex/vite.config.ts
+++ frontend/dockerex/vite.config.ts
@@ -1,7 +1,16 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8084',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
